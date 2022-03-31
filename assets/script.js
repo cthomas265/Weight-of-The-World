f606745976b77af5a47473e430b997898b1cbecd
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-03-30 19:30:32 -0500

    scheduler

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,33 @@
+var time = {};
 
-var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+var rightNow = moment().format("MMM Do YY");
+
+var Time = function () {
+    $("#currentDay").append(rightNow);
+}
+
+Time ();
+
+
+// //convert to moment object
+// var auditTask = function(taskEl) {
+//     var date = $(taskEl)
+//         .find("span")
+//         .text()
+//         .trim();
+    
+//     var currentTime = moment(date, "L").set("hour", 17);
+    
+//     // if time is in the past, add "badge-danger" class
+//     if (time.isBefore(moment())) {
+//         $(taskEl)
+//         .find("span")
+//         .addClass("badge-danger");
+//     }
+//     else if (Math.abs(moment().diff(time, "days")) <= 2) {
+//         $(taskEl).addClass("list-group-item-warning");
+//     }
+// };
+
